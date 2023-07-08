0042e5504144e3a711e6ac99e35ff23c61c395dc
tushargupta97 <97527569+tushargupta97@users.noreply.github.com>
2023-07-08 16:29:51 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,44 @@
+public class Search {
+    public static int search(int arr[],int tar,int si, int ei){
+        //base case
+        if(si>ei){
+            return -1;
+        }
+        //kaam
+        int mid=si+(ei-si)/2;
+        //case FOUND
+        if(arr[mid]==tar){
+            return mid;
+        }
+        //mid on L1
+        if(arr[si]<=arr[mid]){
+            //case a:left
+            if(arr[si]<=tar&&tar<=arr[mid]){
+                return  search(arr,tar,si,mid);
+            }
+            //case b:left
+            else{
+                return search(arr,tar,mid+1,ei);
+            }
+            
+        }
+        //mid on line2
+        else{
+            //case c
+            if(arr[mid]<=tar&&tar<=arr[ei]){
+                return search(arr,tar,mid+1,ei);
+            }
+            //case d
+            else{
+                return search(arr,tar,si,mid-1);
+            }
+        }
+        
+    }
+    public static void main(String args[]){
+        int arr[]={4,5,6,7,0,1,2,3};
+        int target =0; //output->4
+        int tarIdx=search(arr,target,0,arr.length-1);
+        System.out.println(tarIdx);
+    }
+}
